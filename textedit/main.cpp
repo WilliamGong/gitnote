f2ffefea59b6316aeede2e72599272518ba9334e
WilliamGong <gtl119@live.com>
2022-01-10 19:22:32 +0800

    FIXED: QDialogButtonbox 没有 i18n

--- textedit/main.cpp
+++ textedit/main.cpp
@@ -7,9 +7,11 @@ int main(int argc, char *argv[])
   QTextCodec::setCodecForLocale(QTextCodec::codecForName("UTF-8"));
     QApplication a(argc, argv);
 
-    QTranslator translator;
+    QTranslator translator, translatorBase;
     translator.load("textedit_zh.qm");
+    translatorBase.load("qt_zh_CN.qm");
     a.installTranslator(&translator);
+    a.installTranslator(&translatorBase);
     Textedit w;
     w.show();
     return a.exec();
